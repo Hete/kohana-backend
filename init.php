2d3d92dd426d0f0f51385f415326a9f842711738
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-06-02 15:44:05 -0400

    Execute as a shutdown function.

--- init.php
+++ init.php
@@ -7,14 +7,35 @@ defined('SYSPATH') or die('No direct script access.');
  * 
  *     php --backend index.php
  * 
+ * Backend will execute as a shutdown function.
+ * 
  * @package Backend
  * @author Hète.ca Team
  * @copyright (c) 2013, Hète.ca Inc.
  */
 if (Kohana::$is_cli) {
-    if (CLI::options('backend')) {
-        Backend::instance()->start();
-        exit(0);
+
+    function _backend() {
+
+        $options = CLI::options('backend');
+
+        switch ($command = Arr::get($options, 'backend')) {
+            case 'start':
+            case NULL:
+                Backend::instance()->start();
+            case 'stop':
+                Backend::instance()->stop();
+            case 'release':
+                Backend::instance()->release();
+                exit(0);
+                break;
+            default:
+                echo "'$command' is not a valid command.\n\tphp index.php --backend=<command>\n\t<command> can be 'start', 'stop' or 'release'\n";
+                exit(1);
+        }
     }
+
+    // Run at shutdown
+    register_shutdown_function('_backend');
 }
 ?>
